774e5aacba935d0f6243350f5d0201136f6369f4
CachingFoX <CachingFoX@mail-hauser.de>
2017-03-22 08:05:57 +0100

    issue #250: remove old code, which sends an request for every waypoint add some comments

--- gc_little_helper_II.user.js
+++ gc_little_helper_II.user.js
@@ -3633,12 +3633,14 @@ var mainGC = function () {
         }
     }
 
+// added elevation to every additional waypoint with shown coordinates    
     if ( 1 /* TODO: settings*/ && (
         is_page("cache_listing") ||
         document.location.href.match(/^https?:\/\/www\.geocaching\.com\/geocache\//) ||
         document.location.href.match(/^https?:\/\/www\.geocaching\.com\/hide\/wptlist.aspx/) ) ) {
 
         try {
+            // returns a jQuery object of the waypoint list
             function getWaypointTable() {
                 var tbl = $("#ctl00_ContentBody_Waypoints");
                 if ( tbl.length<=0 ) {
@@ -3672,8 +3674,6 @@ var mainGC = function () {
                         addElevationToWaypoint( tbl, index+1 );
                     }
 
-                } else {
-                    console.log("end of waypoint table")
                 }
             }
 
@@ -3730,73 +3730,6 @@ var mainGC = function () {
 
                     }
                 });
-        } catch(e) {
-            gclh_error( "AddElevation: " + e);
-        }
-    }
-
-    if ( 0 /* TODO: settings*/ && (
-        is_page("cache_listing") ||
-        document.location.href.match(/^https?:\/\/www\.geocaching\.com\/geocache\//) ||
-        document.location.href.match(/^https?:\/\/www\.geocaching\.com\/hide\/wptlist.aspx/) ) ) {
-
-        try {
-            function getWaypointTable() {
-                var tbl = $("#ctl00_ContentBody_Waypoints");
-                if ( tbl.length<=0 ) {
-                    tbl = $("#ctl00_ContentBody_WaypointList");
-                }
-                return tbl;
-            }
-
-            function addElevationToWaypoint( tbl, index ) {
-                if ( tbl.find("tbody > tr").length/2 > index ) {
-                    var row1st = tbl.find("tbody > tr").eq(index*2);
-                    var cellCoordinates = row1st.find("td:eq(6)");
-                    var cellElevation = row1st.find("td:eq(7)");
-                    var tmp_coords = toDec(cellCoordinates.text().trim());
-                    if(typeof tmp_coords[0] !== 'undefined' && typeof tmp_coords[1] !== 'undefined') {
-                        GM_xmlhttpRequest({
-                            method: 'GET',
-                            url: "https://maps.googleapis.com/maps/api/elevation/json?sensor=false&locations=" + tmp_coords[0]+","+tmp_coords[1],
-                            onload: function(responseDetails) {
-                                var heightString = "";
-                                var json = JSON.parse(responseDetails.responseText);
-
-                                if (typeof json.results[0].elevation !== "number") {
-                                    heightString = "n/a";
-                                } else {
-                                    var height = json.results[0].elevation; 
-                                    heightString = (height >= 0) ? " +" : " ";
-                                    heightString += Math.round(height) + "m";                                    
-                                }
-                                cellElevation.html( t );
-                                addElevationToWaypoint( tbl, index+1 );
-                            }
-                        });
-                    } else {
-                        cellElevation.html( "???" );
-                        addElevationToWaypoint( tbl, index+1 );
-                    }
-
-                } else {
-                    console.log("end of waypoint table")
-                }
-            }
-
-
-            var tbl = getWaypointTable();
-            tbl.find("thead > tr > th:eq(6)").after('<th scope="col">Elevation</th>'); // added header Elevation after Coordinate
-            var length = tbl.find("tbody > tr").length;
-            for ( var i=0; i<length/2; i++ ) {
-                var row1st = tbl.find("tbody > tr").eq(i*2);
-                var row2nd = tbl.find("tbody > tr:eq("+(i*2+1)+")");
-                var cellNote = row2nd.find("td:eq(2)");
-                var colspan = cellNote.attr('colspan');
-                cellNote.attr('colspan',colspan+1);
-                row1st.find("td:eq(6)").after('<td>...</td>');
-            }
-            addElevationToWaypoint( tbl, 0 );
         } catch(e) {
             gclh_error( "AddElevation: " + e);
         }
