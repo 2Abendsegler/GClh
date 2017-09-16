22e9532e35e2694f4b63978f77ae01f06be5592e
Christian Ruhmer <christian.ruhmer@constancy.de>
2017-09-16 11:18:46 +0200

    Moved Hide/Show All Cachetyp Links below the cachetypes

--- gc_little_helper_II.user.js
+++ gc_little_helper_II.user.js
@@ -4579,11 +4579,11 @@ var mainGC = function () {
                 cacheTypes.forEach(showCacheType); 
             } 
  
-            var ul = document.getElementById("m_myCaches"); 
+            var ul = document.getElementById("m_cacheTypes"); 
             var li = document.createElement("li"); 
  
             var a = document.createElement('a'); 
-            a.appendChild(document.createTextNode("Hide all Caches")); 
+            a.appendChild(document.createTextNode("Hide all Cachetypes")); 
             a.title = "Hide all Caches"; 
             a.href = "#"; 
             li.appendChild(a); 
@@ -4594,7 +4594,7 @@ var mainGC = function () {
  
             li = document.createElement("li"); 
             a = document.createElement('a'); 
-            a.appendChild(document.createTextNode("Show all Caches")); 
+            a.appendChild(document.createTextNode("Show all Cachetypes")); 
             a.title = "Show all Caches"; 
             a.href = "#"; 
             li.appendChild(a); 
