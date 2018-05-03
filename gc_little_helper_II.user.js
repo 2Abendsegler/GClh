06eebb735ca00df3956fc33cf6df162771a443f6
Christian Ruhmer <christian.ruhmer@constancy.de>
2018-05-03 10:29:33 +0200

    Increased Version number

--- gc_little_helper_II.user.js
+++ gc_little_helper_II.user.js
@@ -2,7 +2,7 @@
 // @name             GC little helper II
 // @namespace        http://www.amshove.net
 //--> $$000
-// @version          0.9.4
+// @version          0.9.5
 //<-- $$000
 // @include          http*://www.geocaching.com/*
 // @include          http*://maps.google.tld/*
