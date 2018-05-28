b3309b73fb779ac327bfac1416a603bedac8eda0
2Abendsegler <2Abendsegler@users.noreply.github.com>
2018-05-28 03:20:55 +0200

    Go live v0.9.7

--- gc_little_helper_II.user.js
+++ gc_little_helper_II.user.js
@@ -2,7 +2,7 @@
 // @name             GC little helper II
 // @namespace        http://www.amshove.net
 //--> $$000
-// @version          0.9.6
+// @version          0.9.7
 //<-- $$000
 // @include          http*://www.geocaching.com/*
 // @include          http*://maps.google.tld/*
