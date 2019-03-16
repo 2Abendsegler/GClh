c8053f686983a30b23069e4e3b8294bc2bcb9062
radlerandi <radlerandi@users.noreply.github.com>
2019-03-16 13:27:41 +0100

    Update gc_little_helper_II.user.js

--- gc_little_helper_II.user.js
+++ gc_little_helper_II.user.js
@@ -2,7 +2,7 @@
 // @name             GC little helper II
 // @namespace        http://www.amshove.net
 //--> $$000
-// @version          0.9.16 andi
+// @version          0.9.16
 //<-- $$000
 // @include          http*://www.geocaching.com/*
 // @include          http*://maps.google.tld/*
