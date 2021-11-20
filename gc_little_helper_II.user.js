3134fdb9e5fd31a5f67e3cb1a590f49278567383
2Abendsegler <2Abendsegler@users.noreply.github.com>
2021-11-20 03:58:34 +0100

    Prepare go live v0.11.8 - Upload js

--- gc_little_helper_II.user.js
+++ gc_little_helper_II.user.js
@@ -2,7 +2,7 @@
 // @name         GC little helper II
 // @description  Some little things to make life easy (on www.geocaching.com).
 //--> $$000
-// @version      0.11.7
+// @version      0.11.8
 //<-- $$000
 // @copyright    2010-2016 Torsten Amshove, 2016-2021 2Abendsegler, 2017-2021 Ruko2010
 // @author       Torsten Amshove; 2Abendsegler; Ruko2010
@@ -12135,10 +12135,10 @@ var mainGC = function() {
         var prop = ' style="border: none; visibility: hidden; width: 2px; height: 2px;" alt="">';
         var code = '<img src="https://s11.flagcounter.com/count2/906f/bg_FFFFFF/txt_000000/border_CCCCCC/columns_6/maxflags_60/viewers_0/labels_1/pageviews_1/flags_0/percent_0/"' + prop;
 //--> $$002
-        code += '<img src="https://c.andyhoppe.com/1631377382"' + prop; // Besucher
-        code += '<img src="https://c.andyhoppe.com/1631377443"' + prop; // Seitenaufrufe
-        code += '<img src="https://www.worldflagcounter.com/hYT"' + prop;
-        code += '<img src="https://s11.flagcounter.com/count2/ioWB/bg_FFFFFF/txt_000000/border_CCCCCC/columns_6/maxflags_60/viewers_0/labels_1/pageviews_1/flags_0/percent_0/"' + prop;
+        code += '<img src="https://c.andyhoppe.com/1637376471"' + prop; // Besucher
+        code += '<img src="https://c.andyhoppe.com/1637376528"' + prop; // Seitenaufrufe
+//        code += '<img src="https://www.worldflagcounter.com/hYT"' + prop;
+        code += '<img src="https://s11.flagcounter.com/count2/JTcs/bg_FFFFFF/txt_000000/border_CCCCCC/columns_6/maxflags_60/viewers_0/labels_1/pageviews_1/flags_0/percent_0/"' + prop;
 //<-- $$002
         div.innerHTML = code;
         side.appendChild(div);
@@ -13292,6 +13292,7 @@ var mainGC = function() {
             html += thanksLineBuild("Bananeweizen",         "",                         false, false, true,  false, false);
             html += thanksLineBuild("sunhillduo",           "Yannick-XY",               false, false, true,  false, false);
             html += thanksLineBuild("ColleIsarco",          "",                         false, false, true,  true,  false);
+            html += thanksLineBuild("Pzi",                  "PetziAt",                  false, false, true,  false, false);
             html += thanksLineBuild("ChristianGK",          "ChristianGK-GC",           false, false, true,  false, false);
             html += thanksLineBuild("ztNFny",               "",                         false, false, true,  true,  true);
             // Bug Reporting alphabetisch.
