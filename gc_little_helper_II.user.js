440ce33639122c2e88cf38be4a8c774f49fdd979
ChristianGK-GC <christiangk.gc@gmail.com>
2022-06-26 16:10:11 +0200

    Automatic go back on old log form
    Automatic go back to Drafts after sending to log with old log form.

--- gc_little_helper_II.user.js
+++ gc_little_helper_II.user.js
@@ -694,6 +694,7 @@ var variablesInit = function(c) {
     c.settings_drafts_old_log_form = getValue("settings_drafts_old_log_form", false);
     c.settings_drafts_log_icons = getValue("settings_drafts_log_icons", true);
     c.settings_drafts_go_automatic_back = getValue("settings_drafts_go_automatic_back", false);
+    c.settings_drafts_go_automatic_back_old_log_form = getValue("settings_drafts_go_automatic_back_old_log_form", false);
     c.settings_listing_hide_external_link_warning = getValue("settings_listing_hide_external_link_warning", false);
     c.settings_listing_links_new_tab = getValue("settings_listing_links_new_tab", false);
 
@@ -6430,7 +6431,10 @@ var mainGC = function() {
         } catch(e) {gclh_error("New drafts page",e);}
     }
     // Automatic back to Drafts after sending log.
-    if (settings_drafts_go_automatic_back && document.location.href.match(/\.com\/geocache\/GC[A-Z0-9]{1,10}\?dluid/)) {
+    if (
+        (settings_drafts_go_automatic_back && document.location.href.match(/\.com\/geocache\/GC[A-Z0-9]{1,10}\?dluid/)) ||
+        (settings_drafts_go_automatic_back_old_log_form && document.location.href.match(/\.com\/seek\/log\.aspx\?PLogGuid=([a-zA-Z0-9-]*)/) && ($('#ctl00_ContentBody_LogBookPanel1_lblErrorMessage')[0].children[0].className.toLowerCase() == "success" ))
+       ) {
         document.location = 'https://www.geocaching.com/account/drafts';
     }
 
@@ -14587,6 +14591,7 @@ var mainGC = function() {
             html += "&nbsp; " + checkboxy('settings_drafts_old_log_form', 'Use old-fashioned log form to log a draft') + "<br>";
             html += "&nbsp; " + checkboxy('settings_drafts_log_icons', 'Show logtype icon instead of text') + "<br>";
             html += checkboxy('settings_drafts_go_automatic_back', 'Automatic go back to Drafts after sending to log with new log form') + "<br>";
+            html += checkboxy('settings_drafts_go_automatic_back_old_log_form', 'Automatic go back to Drafts after sending to log with old log form') + "<br>";
             html += newParameterVersionSetzen('0.11') + newParameterOff;
             html += "</div>";
 
@@ -15894,6 +15899,7 @@ var mainGC = function() {
                 'settings_drafts_old_log_form',
                 'settings_drafts_log_icons',
                 'settings_drafts_go_automatic_back',
+                'settings_drafts_go_automatic_back_old_log_form',
                 'settings_listing_hide_external_link_warning',
                 'settings_listing_links_new_tab',
             );
