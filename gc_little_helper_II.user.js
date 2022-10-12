4feda0c3f6fe093adf2280695f3580740475e312
ChristianGK-GC <christiangk.gc@gmail.com>
2022-10-12 21:18:45 +0200

    Update gc_little_helper_II.user.js
    removed unneeded brackets

--- gc_little_helper_II.user.js
+++ gc_little_helper_II.user.js
@@ -1170,11 +1170,11 @@ var mainCertitudes = function() {
                     output += '<br>Info: ' + information;
                 }
                 if (document.getElementsByTagName('a')[5]) {
-                    if ((document.getElementsByTagName('a')[5].children[0]) && (document.getElementsByTagName('a')[5].children[0].tagName == 'IMG')) {
+                    if (document.getElementsByTagName('a')[5].children[0] && document.getElementsByTagName('a')[5].children[0].tagName == 'IMG') {
                         var spoiler = document.getElementsByTagName('a')[5].children[0].src;
                         output += '<br>Spoiler: ' + spoiler;
                     }
-                    if ((document.getElementsByTagName('a')[5].href) && (document.getElementsByTagName('a')[5].download)) {
+                    if (document.getElementsByTagName('a')[5].href && document.getElementsByTagName('a')[5].download) {
                         var fileurl = document.getElementsByTagName('a')[5].href;
                         var filename = document.getElementsByTagName('a')[5].download;
                         output += '<br>' + filename + ': ' + fileurl;
