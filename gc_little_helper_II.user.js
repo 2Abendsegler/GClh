1e50f37781700b7f915a76f9916b47a907c99cad
2Abendsegler <2Abendsegler@users.noreply.github.com>
2023-02-10 16:41:07 +0100

    Upload js mit test1

--- gc_little_helper_II.user.js
+++ gc_little_helper_II.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         GC little helper II
-// @description  Some little things to make life easy (on www.geocaching.com) .
+// @description  Some little things to make life easy (on www.geocaching.com). Test1
 //--> $$000
 // @version      0.14.1
 //<-- $$000
