55eb51a7216a17f1d21c493636adb20b41ed9e21
capoaira <capoaira@web.de>
2023-02-26 21:53:03 +0100

    Use TextEncoder

--- gc_little_helper_II.user.js
+++ gc_little_helper_II.user.js
@@ -18788,11 +18788,16 @@ String.prototype.gcCodeToID = function () {
 
 // Simple a quick schnaader's checksum function.
 String.prototype.checksum = function () {
-    let seed = 0x12345678;
-    for (let i = 0; i < this.length; i++) {
-        seed += (this.charCodeAt(i) * (i + 1));
-    }
-    return (seed & 0xffffffff).toString(16);
+    try {
+        // We use the TextEncoder to use UFT-8, so the seed will never get out of range
+        let encoder = new TextEncoder();
+        let data = encoder.encode(this);
+        let seed = 0x12345678;
+        for (let i = 0; i < data.length; i++) {
+            seed += (data[i] * (i + 1));
+        }
+        return (seed & 0xffffffff).toString(16);
+    } catch (e) {gclh_error("Error bulding checksum", e);}
 }
 
 Date.prototype.getWeekday = function(short = false) {
