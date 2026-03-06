d9e6d3b8ac1000e0e5ac830b65728d4ea613d17f
DieBatzen <16579282+DieBatzen@users.noreply.github.com>
2026-03-06 23:17:56 +0100

    [General] Misaligned header content

--- gc_little_helper_II.user.js
+++ gc_little_helper_II.user.js
@@ -1769,6 +1769,10 @@ var mainGC = function() {
                 // Alle Seiten: Grundeinstellungen:
                 // ----------
                 var css = "";
+                // Alignment of menu content (links and menus); a:height ensures that the submenus remain open when hovering over with the mouse.
+                css += "gclh_nav ul.menu {display: flex;}";
+                css += "gclh_nav ul.menu>li>a {height: 60px; display: flex !important; align-items: center;}";
+                css += ".menu>li {height: unset;}";
                 // Font-Size für Menüs, Font-Size für Untermenüs in Pixel.
                 var font_size_menu = parseInt(settings_font_size_menu);
                 if ((font_size_menu == 0) || (font_size_menu < 0) || (font_size_menu > 16)) font_size_menu = 16;
@@ -1837,15 +1841,11 @@ var mainGC = function() {
                     ".#m .#sm {margin-left: 0 !important}";
                 // Vertikales Menü ausrichten.
                 if (settings_bookmarks_top_menu) {
-                    // Menüzeilenhöhe, Menü nicht flex.
+                    // Menüzeilenhöhe.
                     css += "ul.#m {line-height: 16px; display: block;}";
-                    // Zwischen Menüname und Submenü keine Lücke lassen, sonst klappts nicht mit einfachem Aufklappen.
-                    css += ".#m li a, .#m li a:link, .#m li a:visited {margin-bottom: 13px;} .#m ul.#sm {margin-top: -6px;}";
-                    // Horizontales Menü ausrichten.
                 } else {
                     // Menüzeilenhöhe.
                     css += "ul.#m {line-height: 16px !important;}";
-                    css += "ul.#m > li{margin-bottom: 2px;}";
                     // Zeilenabstand in Abhängigkeit von Anzahl Zeilen.
                     if      (settings_menu_number_of_lines == 2) css += "ul.#m li a {padding-top: 4px !important; padding-bottom: 4px !important;}";
                     else if (settings_menu_number_of_lines == 3) css += "ul.#m li a {padding-top: 1px !important; padding-bottom: 1px !important;}";
@@ -1858,7 +1858,7 @@ var mainGC = function() {
                     changeGcLogo(side);
                 }
                 css +=
-                    "#l {flex: unset; overflow: unset; margin-left: -32px} #newgclogo {width: 30px !important;}" +
+                    "#l {flex: none; flex-basis: 32px; overflow: unset; margin-left: -32px} #newgclogo {width: 30px !important;}" +
                     ".#m {width: " + new_width_menu + "px !important; margin-left: 6px !important;}" +
                     "nav .wrapper, gclh_nav .wrapper {min-width: " + (new_width + 40) + "px !important; max-width: unset;}";
                 // Bereich links ausrichten in Abhängigkeit davon, ob Logo geändert wird und ob GC Tour im Einsatz ist.
@@ -1873,25 +1873,19 @@ var mainGC = function() {
                     css += "nav .wrapper {padding-right: " + new_padding_right + "px !important; width: unset;}";
                     // Vertikales Menü ausrichten.
                     if (settings_bookmarks_top_menu) {
-                        if (is_page("find_cache") || (is_page("settings") && document.location.pathname.match(/^\/live\//))) {
-                            css += ".#m li:not(.attention-link-parent) ul.#sm {margin-top: 17px;}";
-                        }
                         css += ".#m ul.#sm {margin-top: 0px; margin-left: 32px !important;} .#m .submenu::after {left: 4px; width: 26px;}";
-                        // Menü, Searchfield ausrichten in Abhängigkeit von Schriftgröße. Menü nicht flex.
+                        // Menü, Searchfield ausrichten.
                         if (settings_menu_float_right) {
-                            css += "ul.#m > li {margin-top: " + (3 + (16 - font_size_menu) / 2) + "px;}";
+                            css += "gclh_nav ul.menu {justify-content: flex-end;}";
                         } else {
-                            if (is_page("map")) css += "ul.#m > li {margin-top: " + (3 + (16 - font_size_menu) / 2) + "px;}";
-                            else css += "ul.#m > li {margin-top: " + (4 + (16 - font_size_menu) / 2) + "px;}";
+                            css += "gclh_nav ul.menu {justify-content: flex-start;}";
                         }
-                        // Menü in Karte ausrichten.
-                        if (is_page("map") && !settings_menu_float_right) css += ".#m {height: unset !important;}";
-                        if (is_page("map") && settings_menu_float_right) css += "#navi_search {margin: 0 !important;}";
                     // Horizontales Menü ausrichten in Abhängigkeit von Anzahl Zeilen.
                     } else {
-                        if      (settings_menu_number_of_lines == 1) css += "ul.#m {top:   4px !important; position: inherit;}";
-                        else if (settings_menu_number_of_lines == 2) css += "ul.#m {top:  -8px !important; position: inherit; flex-wrap: wrap;}";
-                        else if (settings_menu_number_of_lines == 3) css += "ul.#m {top: -13px !important; position: inherit; flex-wrap: wrap;}";
+                        css += ".#m ul.#sm {margin-top: 0px; margin-left: 32px !important;} .#m .submenu::after {left: 4px; width: 26px;}";
+                        if      (settings_menu_number_of_lines == 1) css += "ul.#m {top:   0px !important; position: inherit;}";
+                        else if (settings_menu_number_of_lines == 2) css += "ul.#m {top:  -8px !important; position: inherit; flex-wrap: wrap;} gclh_nav ul.menu>li>a {height: auto !important}";
+                        else if (settings_menu_number_of_lines == 3) css += "ul.#m {top: -13px !important; position: inherit; flex-wrap: wrap;} gclh_nav ul.menu>li>a {height: auto !important}";
                     }
                     // Prevent the user area in the header from moving to the left on narrow screens.
                     css += "ul.menu {order: unset;} #ctl00_uxLoginStatus_divSignedIn {height: auto;} .messagecenterheaderwidget.li-messages {position: static;}";
@@ -1907,24 +1901,19 @@ var mainGC = function() {
                     }
                     // Vertikales Menü  ausrichten.
                     if (settings_bookmarks_top_menu) {
-                        if (is_page("cache_listing") && $('.ul__cache-details.unstyled')[0]) {
-                            css += ".#m ul.#sm {margin-top: -10px; margin-left: 32px !important;} .#m .submenu::after {left: 4px; width: 26px;}";
-                        } else {
-                            css += ".#m ul.#sm {margin-top: 17px; margin-left: 32px !important;} .#m .submenu::after {left: 4px; width: 26px;}";
-                        }
-                        // Zwischen Menüname und Submenü keine Lücke lassen, sonst klappt das nicht mit dem einfachen Aufklappen.
-                        css += ".#m > li .dropdown {padding-bottom: 14px !important;}";
-                        // Menü, Searchfield ausrichten in Abhängigkeit von Schriftgröße. Menü nicht flex.
+                        css += ".#m ul.#sm {margin-top: 0px; margin-left: 32px !important;} .#m .submenu::after {left: 4px; width: 26px;}";
+                        // Menü, Searchfield ausrichten.
                         if (settings_menu_float_right) {
-                            css += "ul.#m > li {margin-top: " + (7 + (16 - font_size_menu) / 2) + "px;}";
+                            css += "gclh_nav ul.menu {justify-content: flex-end;}";
                         } else {
-                            css += "ul.#m > li {margin-top: " + (5 + (16 - font_size_menu) / 2) + "px;}";
+                            css += "gclh_nav ul.menu {justify-content: flex-start;}";
                         }
                     // Horizontales Menü ausrichten in Abhängigkeit von Anzahl Zeilen.
                     } else {
-                        if      (settings_menu_number_of_lines == 1) css += "ul.#m {top:   4px !important; position: inherit;}";
-                        else if (settings_menu_number_of_lines == 2) css += "ul.#m {top:  -8px !important; position: inherit; flex-wrap: wrap;}";
-                        else if (settings_menu_number_of_lines == 3) css += "ul.#m {top: -13px !important; position: inherit; flex-wrap: wrap;}";
+                        css += ".#m ul.#sm {margin-top: 0px; margin-left: 32px !important;} .#m .submenu::after {left: 4px; width: 26px;}";
+                        if      (settings_menu_number_of_lines == 1) css += "ul.#m {top:   0px !important; position: inherit;}";
+                        else if (settings_menu_number_of_lines == 2) css += "ul.#m {top:  -8px !important; position: inherit; flex-wrap: wrap;} gclh_nav ul.menu>li>a {height: auto !important}";
+                        else if (settings_menu_number_of_lines == 3) css += "ul.#m {top: -13px !important; position: inherit; flex-wrap: wrap;} gclh_nav ul.menu>li>a {height: auto !important}";
                     }
                 }
                 // Alle Seiten: Platzhalter umsetzen:
@@ -2124,7 +2113,7 @@ var mainGC = function() {
                     code += "  else document.location.href = '/seek/nearest.aspx?navi_search='+search;";
                     code += "}";
                     injectPageScript(code, "body");
-                    var searchfield = "<li><input onKeyDown='if (event.keyCode==13 && event.altKey == false && event.shiftKey == false) {gclh_search_logs(!!event.ctrlKey); return false;}' type='text' size='" + (browser == 'firefox' ? "9":"7") + "' name='navi_search' id='navi_search' style='margin-bottom: -1px; padding: 1px; font-weight: bold; font-family: sans-serif; border-radius: 7px 7px 7px 7px;' value='" + settings_bookmarks_search_default + "'></li>";
+                    var searchfield = "<li><input onKeyDown='if (event.keyCode==13 && event.altKey == false && event.shiftKey == false) {gclh_search_logs(!!event.ctrlKey); return false;}' type='text' size='" + (browser == 'firefox' ? "9":"7") + "' name='navi_search' id='navi_search' style='margin-bottom: -1px; margin-top: 0px; padding: 1px; font-weight: bold; font-family: sans-serif; border-radius: 7px 7px 7px 7px;' value='" + settings_bookmarks_search_default + "'></li>";
                     $(".Menu, .menu").append(searchfield);
                 }
 
@@ -2136,18 +2125,6 @@ var mainGC = function() {
                             var separator = document.createElement("li");
                             separator.appendChild(document.createTextNode("|"));
                             menuChilds[i].parentNode.insertBefore(separator, menuChilds[i]);
-                        }
-                    }
-                }
-                // Vertikale Menüs rechts ausrichten.
-                if (settings_bookmarks_top_menu && settings_menu_float_right && settings_change_header_layout) {
-                    if ($('ul.Menu, ul.menu')[0]) {
-                        var menu = $('ul.Menu, ul.menu')[0];
-                        var menuChilds = $('ul.Menu, ul.menu')[0].children;
-                        for (var i = 0; i < menuChilds.length; i++) {
-                            var child = menu.removeChild(menu.children[menuChilds.length-1-i]);
-                            child.setAttribute("style", "float: right;");
-                            menu.appendChild(child);
                         }
                     }
                 }
